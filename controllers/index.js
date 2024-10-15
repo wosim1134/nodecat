e5137d08d2e7e543b8c7059f1f7e1c867dd6b50a
wosim1134 <pubglnar@gmail.com>
2024-10-15 12:26:09 +0900

    nodecat 수정 241015

--- controllers/index.js
+++ controllers/index.js
@@ -1,41 +1,52 @@
 const axios = require('axios');
-const { process_params } = require('express/lib/router');
 //
 
-exports.test = async (req, res, next) => {
+const URL = process.env.API_URL; //
+axios.defaults.headers.origin = process.env.ORIGIN; //
+
 //
-//
+const request = async (req, api) => {
     try {
         if (!req.session.jwt) { //
-            const tokenResult = await axios.post('http://localhost:8002/v1/token', {
+            const tokenResult = await axios.post(`${URL}/token`, { //
                 clientSecret: process.env.CLIENT_SECRET,
-                //
             });
-            if (tokenResult.data?.code === 200) {
-            //
-                req.session.jwt = tokenResult.data.token;
-                //
-            } else {
-            //
-                return res.json(tokenResult.data);
-                //
-            }
+            req.session.jwt = tokenResult.data.token; //
         }
-        //
-        const result = await axios.get('http://localhost:8002/v1/test', {
-            headers: { authorization: req.session.jwt },
-            //
+        return await axios.get(`${URL}${api}`, { //
+            headers: { authorization: req.session.jwt }, //
         });
-        return res.json(result.data);
-        //
-    } catch (err) {
-        console.error(err);
-        //
-        if (err.response?.status === 419) {
-        //
-            return res.json(err.response.data);
+    } catch (error) {
+        if (error.response?.status === 419) { //
+            delete req.session.jwt; //
+            return request(req, api); //
         }
-        return next(err);
-        //
+        throw error; //
+    }
+};
+
+//
+exports.getMyPosts = async (req, res, next) => {
+    try {
+        const result = await request(req, '/posts/my'); //
+        res.json(result.data); //
+    } catch(error) {
+        console.error(error); //
+        next(error); //
+    }
+};
+
+//
+exports.searchByHashtag = async (req, res, next) => {
+    try {
+        const result = await request(
+            req, `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`, //
+        );
+        res.json(result.data); //
+    } catch(error) {
+        if (error.code) { //
+            console.error(error); //
+            next(error); //
+        }
     }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
 const express = require('express'); //
-const { test } = require('../controllers'); 
+const { searchByHashtag, getMyPosts } = require('../controllers'); 
 //
 
 const router = express.Router();
 //
 
 //
-router.get('/test', test);
+router.get('/myposts', getMyPosts);
+//
+
+//
+router.get('/search/:hashtag', searchByHashtag);
 //
 
 module.exports = router;
+//
